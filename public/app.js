cfd685a21672ce4859ad5917c0c8807ee3f2aef3
finlay brydges <100164471+finlay-ab@users.noreply.github.com>
2024-09-12 18:38:09 +0100

    Added anylitics for when a user makes a calculation

--- public/app.js
+++ public/app.js
@@ -1,21 +1,23 @@
-
 document.getElementById('textInput').addEventListener('input', function() {
     let value = this.value;
     let slider = document.getElementById('slider');
     slider.value = value;
   });
   
-  document.getElementById('slider').addEventListener('input', function() {
-    let value = this.value;
-    let textInput = document.getElementById('textInput');
-    textInput.value = value;
-  });
-  function calculateLoan() {
-    let loanAmount = document.getElementById('slider').value;
-    let monthlyPayment = loanAmount * 0.002;
-    let yearlyPayment = loanAmount * 0.024;
-  
-    document.getElementById('monthlyPayment').innerText = `Monthly Payment: £${monthlyPayment.toFixed(2)}`;
-    document.getElementById('yearlyPayment').innerText = `Yearly Payment: £${yearlyPayment.toFixed(2)}`;
-  }
+document.getElementById('slider').addEventListener('input', function() {
+  let value = this.value;
+  let textInput = document.getElementById('textInput');
+  textInput.value = value;
+});
+
+function calculateLoan() {
+  let loanAmount = document.getElementById('slider').value;
+  let monthlyPayment = loanAmount * 0.002;
+  let yearlyPayment = loanAmount * 0.024;
+
+  document.getElementById('monthlyPayment').innerText = `Monthly Payment: £${monthlyPayment.toFixed(2)}`;
+  document.getElementById('yearlyPayment').innerText = `Yearly Payment: £${yearlyPayment.toFixed(2)}`;
+}
+
+
   
